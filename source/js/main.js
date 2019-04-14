3ea7ab3e2bb3bc66c421e24e0260bcda71b06a85
kadonomaro <denis.afer@gmail.com>
2019-04-14 22:02:29 +0700

    added responsive

--- source/js/main.js
+++ source/js/main.js
@@ -15,6 +15,23 @@ $(document).ready(function () {
         speed: 500,
         slidesToShow: 5,
         slidesToScroll: 3,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        responsive: [
+            {
+              breakpoint: 1023,
+              settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1
+              }
+            }]
     });
     
     $('.js-blog-slider').slick({
@@ -24,6 +41,21 @@ $(document).ready(function () {
         speed: 500,
         slidesToShow: 3,
         slidesToScroll: 1,
+        responsive: [
+            {
+              breakpoint: 1023,
+              settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+              }
+            },
+            {
+              breakpoint: 600,
+              settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+              }
+            }]
     });
     
 
@@ -33,5 +65,10 @@ $(document).ready(function () {
         $('.main-nav').toggleClass('main-nav--active');
         
     });
+
+    $(window).scroll(function () { 
+        $('.main-header').addClass('main-header--white');
+    });
+
 	
 });
